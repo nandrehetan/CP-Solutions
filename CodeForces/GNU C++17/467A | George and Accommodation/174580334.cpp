dc34373a19f2c6a0b5a78b35bc53fcd6b419e65a
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2023-03-21 02:52:32 +0530

    Committing files

---
+++ CodeForces/GNU C++17/467A | George and Accommodation/174580334.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include<math.h>
+using namespace std;
+#define ll long long 
+#define ASCII_SIZE 256
+//Hetan Nandre's code 
+   
+ 
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    #ifndef ONLINE_JUDGE 
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+    #endif
+ 
+    int n;
+    cin >> n;
+    int ans = 0;
+    while (n--)
+    {
+        int p, q;
+        cin >> p >> q;
+        if (abs(p-q) >= 2)
+        {
+            ans++;
+        }
+        else
+        {
+            continue;
+        }
+        
+    }
+    cout << ans << endl;
+    return 0;
+}
