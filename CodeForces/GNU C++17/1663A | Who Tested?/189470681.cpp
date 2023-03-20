c5203c69cd3f840e32c5c6a5228be9e66c87201f
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2023-03-21 02:11:29 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1663A | Who Tested?/189470681.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+   cout << "BucketPotato" << endl;
+    
+}
+ 
+ 
+ 
+ 
+ 
+   
+   
+ 
+    
+    
+ 
+ 
+ 
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    t=1;
+    while(t--){
+        solve();
+    
+    }
+ 
+ 
+ 
+    return 0;
+}
