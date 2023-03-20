ce7716a23453d79241905e4901ed14e2db3b43b2
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2023-03-21 02:56:44 +0530

    Committing files

---
+++ CodeForces/GNU C++17/200B | Drinks/171565732.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+    #endif
+ 
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    double sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + (a[i]/100.00);
+    }
+    cout << (sum / n) * 100 << endl;
+    
+}
