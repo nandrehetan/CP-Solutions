63f739191a51a8ef1836b03a75e5373d795a5e61
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2023-03-21 02:10:30 +0530

    Committing files

---
+++ CodeForces/Java 11/1351A | A+B (Trial Problem)/189778023.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ 
+public class L2 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0 ; i< t;i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
