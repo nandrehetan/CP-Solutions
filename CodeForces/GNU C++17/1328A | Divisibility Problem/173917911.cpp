9be5783a57d526e08fadace4584ac73ef05ad510
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2023-03-21 02:53:46 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1328A | Divisibility Problem/173917911.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include<math.h>
+using namespace std;
+#define ll long long 
+#define ASCII_SIZE 256
+//Hetan Nandre's code 
+ 
+ 
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    #ifndef ONLINE_JUDGE 
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+    #endif
+ 
+    
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll a, b;
+        cin >> a >> b;
+        // ll x = 0;
+        if (a%b==0)
+        {
+            cout << 0 << endl;
+        }
+        else
+        {
+            cout << b - a%b << endl;
+        }
+        
+    }
+    
+    
+    
+    return 0;
+    
+}
