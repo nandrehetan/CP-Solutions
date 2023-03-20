c8340228105cc2161d98df77519b193f8b6a6f86
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2023-03-21 02:05:40 +0530

    Committing files

---
+++ CodeForces/Kotlin 1.6/1211A | Three Problems/191702391.kt
@@ -0,0 +1,21 @@
+private fun readLn() = readLine()!! // string line
+private fun readInt() = readLn().toInt() // single int
+private fun readLong() = readLn().toLong() // single long
+private fun readStrings() = readLn().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
+ 
+fun main(args: Array<String>) {
+    var n = readInt()
+    var a = readInts()
+    for (i in 0..n-1) {
+        var j = 0
+        var k = 0
+        while (j < n && a[j] >= a[i]) j++
+        while (k < n && a[k] <= a[i]) k++
+        if (j < n && k < n) {
+            println("${j+1} ${i+1} ${k+1}")
+            return
+        }
+    }
+    println("-1 -1 -1")
+}
