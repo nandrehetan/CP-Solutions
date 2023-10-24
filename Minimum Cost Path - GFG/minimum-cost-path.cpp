6eccde127abbc6a05d3d2691417bbd79d47b8914
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2023-10-24 23:41:05 +0530

    Added solution - LeetHub

--- Minimum Cost Path - GFG/minimum-cost-path.cpp
+++ Minimum Cost Path - GFG/minimum-cost-path.cpp
@@ -5,47 +5,50 @@ using namespace std;
 // } Driver Code Ends
 
 
-class Solution {
+class Solution
+{
 public:
-    bool isValid(int n, int m, int i, int j) {
-        return (i >= 0 && j >= 0 && i < n && j < m);
-    }
-
-    int minimumCostPath(vector<vector<int>>& grid) {
+    int minimumCostPath(vector<vector<int>>& grid) 
+    {
         int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, INT_MAX));
+        if (n == 0) {
+            return 0;
+        }
+
+        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
+        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+
         dp[0][0] = grid[0][0];
+        pq.push({grid[0][0], {0, 0}});
 
-        set<pair<int, pair<int, int>>> s;
-        s.insert({grid[0][0], {0, 0}});
+        while (!pq.empty()) {
+            auto curr = pq.top();
+            pq.pop();
+            int currCost = curr.first;
+            int i = curr.second.first;
+            int j = curr.second.second;
 
-        int dx[] = {0, 0, 1, -1};
-        int dy[] = {1, -1, 0, 0};
+            if (currCost > dp[i][j]) {
+                continue;
+            }
 
-        while (!s.empty()) {
-            auto it = s.begin();
-            int x = it->second.first;
-            int y = it->second.second;
-            int curr_cost = it->first;
-            s.erase(it);
-
-            for (int k = 0; k < 4; k++) {
-                int new_x = x + dx[k];
-                int new_y = y + dy[k];
-                if (isValid(n, m, new_x, new_y) && dp[new_x][new_y] > dp[x][y] + grid[new_x][new_y]) {
-                    if (dp[new_x][new_y] != INT_MAX) {
-                        s.erase(s.find({dp[new_x][new_y], {new_x, new_y}}));
+            for (auto& dir : directions) {
+                int new_i = i + dir.first;
+                int new_j = j + dir.second;
+                if (new_i >= 0 && new_i < n && new_j >= 0 && new_j < n) {
+                    int newCost = currCost + grid[new_i][new_j];
+                    if (newCost < dp[new_i][new_j]) {
+                        dp[new_i][new_j] = newCost;
+                        pq.push({newCost, {new_i, new_j}});
                     }
-                    dp[new_x][new_y] = dp[x][y] + grid[new_x][new_y];
-                    s.insert({dp[new_x][new_y], {new_x, new_y}});
                 }
             }
         }
-        return dp[n - 1][m - 1];
+
+        return dp[n-1][n-1];
     }
 };
-
 
 //{ Driver Code Starts.
 int main(){
