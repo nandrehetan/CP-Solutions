fddecfe7c54ba38a1be52ab4e4787e551d7f8fb6
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2023-11-04 22:15:47 +0530

    Added solution - LeetHub

--- Set all the bits in given range of a number - GFG/set-all-the-bits-in-given-range-of-a-number.cpp
+++ Set all the bits in given range of a number - GFG/set-all-the-bits-in-given-range-of-a-number.cpp
@@ -5,48 +5,14 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    string trimLeadingZeros( string binaryStr) {
-    int pos = binaryStr.find_first_not_of('0');
-    if (pos != string::npos) {
-        return binaryStr.substr(pos);
-    }
-    return "0";  // If the binaryStr is all zeros, return "0".
-}
+    int setAllRangeBits(int N , int L , int R) {
+        // code here
+        int mask = ((1 << (R - L + 1)) - 1) << (L - 1);
 
- int binaryToDecimal(string binaryStr) {
-    int decimalValue = 0;
-    int base = 1;  // Initialize the base for the rightmost bit (2^0).
+    // Perform a bitwise OR operation with N
+    int result = N | mask;
 
-    for (int i = binaryStr.size() - 1; i >= 0; --i) {
-        if (binaryStr[i] == '1') {
-            decimalValue += base;
-        }
-        base *= 2;  // Move to the next higher power of 2.
-    }
-
-    return decimalValue;
-}
- 
-
-
-    int setAllRangeBits(int n , int l , int r) {
-
-      string sol1 = bitset<30>(n).to_string();
-      string sol = trimLeadingZeros(sol1);
-
-       if (l > sol.size() || r > sol.size()) {
-        return n;  // Return the original value if the range is out of bounds.
-    }
-      int L  = sol.size()-l;
-      int R = sol.size() - r;
-
-      for(int i =R;i<=L;i++){
-          sol[i] = '1';
-      }
-
-      int dec = binaryToDecimal(sol);
-      return dec;
-
+    return result;
     }
 };
 
