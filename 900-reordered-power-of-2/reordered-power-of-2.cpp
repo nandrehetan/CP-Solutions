e897f32d00b6b590a10f65a4240ae1b15e06e4f3
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2023-12-18 19:24:46 +0530

    Time: 3 ms (56.76%) | Memory: 6.7 MB (33.98%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -2,19 +2,14 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int n) {
-        // Count the digits of n
-        std::vector<int> countN = countDigits(n);
-        
-        // Check all powers of 2 with the same number of digits as n
-        for (int i = 0; i < 31; ++i) {  // There are at most 31 powers of 2 with 32-bit integer
+        std::vector<int> countN = countDigits(n);        
+        for (int i = 0; i < 31; ++i) {
             if (countDigits(1 << i) == countN) {
                 return true;
             }
         }
-        
         return false;
     }
-    
 private:
     std::vector<int> countDigits(int num) {
         std::vector<int> count(10, 0);
