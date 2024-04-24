7ad9d8cf831d1c4ac00cfcb6bb127bab56dc1282
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2024-04-24 17:48:17 +0530

    Time: 4 ms (33.32%) | Memory: 12.1 MB (10.47%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int r=matrix.size();
-        int c=matrix[0].size();
         for(auto it:matrix){
             bool bs=binary_search(it.begin(),it.end(),target);
             if(bs){
