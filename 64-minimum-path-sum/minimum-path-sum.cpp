77b6db32cf25d26b18d33e6e47b3b94c57d1fd03
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2024-04-28 11:49:34 +0530

    Time: 7 ms (59.24%) | Memory: 12.2 MB (80.52%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,3 +1,10 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
